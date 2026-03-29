314a1ff2c275fe7407101aedb292e0fe6fc8f582
AkaHarshit <104684660+AkaHarshit@users.noreply.github.com>
2026-03-29 20:25:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (42.69%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
         return (
-            sorted([s1[0], s1[2]]) == sorted([s2[0], s2[2]]) and
-            sorted([s1[1], s1[3]]) == sorted([s2[1], s2[3]])
+            (s1[0] == s2[0] and s1[2] == s2[2] or s1[0] == s2[2] and s1[2] == s2[0]) and
+            (s1[1] == s2[1] and s1[3] == s2[3] or s1[1] == s2[3] and s1[3] == s2[1])
         )
